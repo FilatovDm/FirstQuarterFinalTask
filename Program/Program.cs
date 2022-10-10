ccba073e52e32b4ca1526e9491d5ed3784a73227
Dmitriy Filatov <filatov-d@mail.ru>
2022-10-10 18:05:05 +0300

    The programm code is written

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,3 +5,24 @@
 лучше обойтись исключительно массивами
 */
 
+string[] GetArrayThreeChars(string[] array)
+{
+    string[] newArray = new string[3];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+
+string[] sourceArray = {"Hello", ",", "world", "!", "Hi!"};
+string[] resultArray = GetArrayThreeChars(sourceArray);
+
+Console.WriteLine("[{0}]", string.Join(", ", resultArray));
